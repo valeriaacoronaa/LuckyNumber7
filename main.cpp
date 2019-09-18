c0c2b651d96831081eff364c113366b456d904d1
valeriaacoronaa <vcorona@mail.csuchico.edu>
2019-09-18 16:12:15 -0700

    allows user to guess my number

--- main.cpp
+++ main.cpp
@@ -12,13 +12,19 @@ int main()
 
 
   //when they guess correctly (7), display:
+  if(guess==7)
+  {
   cout<<"YES! Lucky number 7!\n";
-
+  }
   //when they guess just off by one (either 6 or 8), display:
+  else if(guess==6 || guess==8)
+  {
   cout<<"Oh, so close!\n";
-
+  }
   //when they guess any other number, display:
+  else
+  {
   cout<<"Waaay off, sorry.\n";
-
+  }
   return 0;
 }
